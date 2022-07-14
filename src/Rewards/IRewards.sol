8bd677dda05921074f82bcff62bb0d9281d6c69e
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-07-14 11:32:40 -0700

    VSCode changes

--- src/Rewards/IRewards.sol
+++ src/Rewards/IRewards.sol
@@ -8,6 +8,10 @@ interface Rewards {
 }
 
 // is debt denominated in tToken or in FIL?
+// how does the transmuter handle "bad debt"?
+
+
+
 /**
   OTC bootstrap (simplified example with no swap actor)
   1 investor and 1 miner and they make OTC deals together
@@ -19,57 +23,171 @@ interface Rewards {
   t=0:
     - Miner: 0 FIL, 0 tTokens
     - Loan agent: 0 FIL, 0 tTokens, debt: 0 FIL
-    - Investor: 900 FIL, 0 tTokens
-    - Rewards: 0 FIL, 0 tTokens
+    - Investor: 900 FIL, 0 tTokens, 0 staked tTokens
+    - Transmuter: 0 FIL, 0 tTokens
     - tToken circulating supply: 0k
 
   miner mints 1000 tTokens as credit (miner calls `mint` on the loan agent)
   t=1:
     - Miner: 0 FIL, 0 tTokens
     - Loan agent: 0 FIL, 1000 tTokens, debt: 1000 FIL
-    - Investor: 900 FIL, 0 tTokens
-    - Rewards: 0 FIL, 0 tTokens
+    - Investor: 900 FIL, 0 tTokens, 0 staked tTokens
+    - Transmuter: 0 FIL, 0 tTokens
     - tToken circulating supply: 1k
 
   miner swaps 1000 tTokens at fixed OTC price with investor and pledges
+  investor immediately stakes 1000 tTokens in the transmuter
   t=2:
     - Miner: 900 FIL, 0 tTokens
     - Loan agent: 0 FIL, 0 tTokens, debt: 1000 FIL
-    - Investor: 0 FIL, 1000 tTokens
-    - Rewards: 0 FIL, 0 tTokens
+    - Investor: 0 FIL, 0 tTokens, 1000 staked tTokens
+    - Transmuter: 0 FIL, 1000 tTokens
     - tToken circulating supply: 1k
 
-  miner gets 1000 FIL in rewards
+  miner gets 1000 FIL in rewards and is available to withdraw
   t=3:
     - Miner: 1900 FIL, 0 tTokens
     - Loan agent: 0 FIL, 0 tTokens, debt: 1000 FIL
-    - Investor: 0 FIL, 1000 tTokens
-    - Rewards: 0 FIL, 0 tTokens
+    - Investor: 0 FIL, 0 tTokens, 1000 staked tTokens
+    - Transmuter: 0 FIL, 1000 tTokens
     - tToken circulating supply: 1k
 
-  miner calls `paydownDebtFromRewards`
+  miner calls `paydownDebtFromRewards` on loan agent,
+  which withdraws the funds from the miner actor and sends them to the transmuter
   t=4:
     - Miner: 900 FIL, 0 tTokens
     - Loan agent: 0 FIL, 0 tTokens, debt: 0 FIL
-    - Investor: 0 FIL, 1000 tTokens
-    - Rewards: 1000 FIL, 0 tTokens
+    - Investor: 0 FIL, 0 tTokens, 1000 staked tTokens
+    - Transmuter: 1000 FIL, 1000 tTokens
     - tToken circulating supply: 1k
 
+  investor calls `claim` on the transmuter
+  t=5:
+    - Miner: 900 FIL, 0 tTokens
+    - Loan agent: 0 FIL, 0 tTokens, debt: 0 FIL
+    - Investor: 1000 FIL, 0 tTokens, 0 staked tTokens
+    - Transmuter: 0 FIL, 0 tTokens
+    - tToken circulating supply: 0
+ */
 
+/**
+  OTC bootstrap (simplified example with no swap actor)
+  1 investor and 1 miner and they make OTC deals together
 
+  the fixed OTC price of tToken/FIL:
+  - 1 FIL = 1.1 tToken
+  - 1 tToken = .9 FIL
 
-
-
+  t=0:
+    - Miner: 0 FIL, 0 tTokens
+    - Loan agent: 0 FIL, 0 tTokens, debt: 0 FIL
+    - Investor: 900 FIL, 0 tTokens, 0 staked tTokens
+    - Transmuter: 0 FIL, 0 tTokens
+    - tToken circulating supply: 0k
 
   miner mints 1000 tTokens as credit (miner calls `mint` on the loan agent)
-  t=5:
+  t=1:
     - Miner: 0 FIL, 0 tTokens
     - Loan agent: 0 FIL, 1000 tTokens, debt: 1000 FIL
-    - Investor: 900 FIL, 0 tTokens
-    - Rewards: 0 FIL, 0 tTokens
+    - Investor: 900 FIL, 0 tTokens, 0 staked tTokens
+    - Transmuter: 0 FIL, 0 tTokens
     - tToken circulating supply: 1k
 
+  miner swaps 1000 tTokens at fixed OTC price with investor and pledges
+  investor immediately stakes 1000 tTokens in the transmuter
+  t=2:
+    - Miner: 900 FIL, 0 tTokens
+    - Loan agent: 0 FIL, 0 tTokens, debt: 1000 FIL
+    - Investor: 0 FIL, 0 tTokens, 1000 staked tTokens
+    - Transmuter: 0 FIL, 1000 tTokens
+    - tToken circulating supply: 1k
+
+  miner gets 100 FIL in rewards and is available to withdraw
+  t=3:
+    - Miner: 1000 FIL, 0 tTokens
+    - Loan agent: 0 FIL, 0 tTokens, debt: 1000 FIL
+    - Investor: 0 FIL, 0 tTokens, 1000 staked tTokens
+    - Transmuter: 0 FIL, 1000 tTokens
+    - tToken circulating supply: 1k
+
+  miner calls `paydownDebtFromRewards` on loan agent,
+  which withdraws the funds from the miner actor and sends them to the transmuter
+  t=4:
+    - Miner: 900 FIL, 0 tTokens
+    - Loan agent: 0 FIL, 0 tTokens, debt: 900 FIL
+    - Investor: 0 FIL, 0 tTokens, 1000 staked tTokens
+    - Transmuter: 100 FIL, 1000 tTokens
+    - tToken circulating supply: 1k
+
+  investor calls `claim` on the transmuter
+  t=5:
+    - Miner: 900 FIL, 0 tTokens
+    - Loan agent: 0 FIL, 0 tTokens, debt: 0 FIL
+    - Investor: 1000 FIL, 0 tTokens, 0 staked tTokens
+    - Transmuter: 0 FIL, 0 tTokens
+    - tToken circulating supply: 0
+
+
+
+
+
+
+
+
+
+Initially expect 2000 FIL in rewards
+2000 total tTokens staked
+100 FIL in the transmuter
+emergency event - no more FIL expected (big miner default)
+investorA has 1000 tTokens
+investorB has 1000 tTokens
+
+investorA claims to get his half (50 FIL)
+how many tokens to burn?
+
+
+
+
+50 FIL in the transmuter
+1000 tokens are staked
+
+Time weighted pro-rata distribution:
+- your share is the time you spent staking * the amount you staked
+- total pool = the total time of everyone thats been staking * the total rewards that came in
+-
+
+
+
+
+
  */
+
+ /**
+  Transmuter as an investment pool
+
+  t=0
+  - Miner mints 100 tToken
+  - investment pool => 0 FIL
+  - rewards pool => 0 FIL
+
+  t=1
+  InvestorA puts in 100 FIL
+  Assume 1FIL = 1tToken
+  InvestorA gets 100 tTokens
+
+
+
+
+
+  InvestorA ownership % = investment / (investment pool + reward pool)
+
+
+
+  */
+
+
+
+
 
 
 /**
