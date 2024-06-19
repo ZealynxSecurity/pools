b2169a892d2df479d932496f4018fd54e296605e
Jonathan <jpschwartz2@uwalumni.com>
2024-06-18 21:50:21 -0700

    Use ERC20Burnable extension

--- src/Token/Token.sol
+++ src/Token/Token.sol
@@ -4,23 +4,22 @@ pragma solidity ^0.8.17;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 import {FilAddress} from "shim/FilAddress.sol";
 import {Ownable} from "src/Auth/Ownable.sol";
 
-contract Token is ERC20, ERC20Permit, ERC20Votes, Ownable {
+contract Token is ERC20, ERC20Permit, ERC20Votes, ERC20Burnable, Ownable {
     using FilAddress for address;
 
     address public minter;
-    address public burner;
 
-    constructor(string memory _name, string memory _symbol, address _owner, address _minter, address _burner)
+    constructor(string memory _name, string memory _symbol, address _owner, address _minter)
         ERC20(_name, _symbol)
         ERC20Permit(_name)
         Ownable(_owner)
     {
         minter = _minter;
-        burner = _burner;
     }
 
     function balanceOf(address account) public view override(ERC20) returns (uint256) {
@@ -48,8 +47,8 @@ contract Token is ERC20, ERC20Permit, ERC20Votes, Ownable {
         super._mint(account.normalize(), value);
     }
 
-    function burn(address account, uint256 value) public {
-        super._burn(account.normalize(), value);
+    function burnFrom(address account, uint256 value) public override {
+        super.burnFrom(account.normalize(), value);
     }
 
     function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
@@ -62,9 +61,5 @@ contract Token is ERC20, ERC20Permit, ERC20Votes, Ownable {
 
     function setMinter(address _minter) external onlyOwner {
         minter = _minter;
-    }
-
-    function setBurner(address _burner) external onlyOwner {
-        burner = _burner;
     }
 }
