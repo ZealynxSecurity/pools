61b491fdfb55ff56414620df05156fdfcf66a8ed
scab24 <git.seco@protonmail.com>
2024-07-11 20:42:09 +0200

    borrow

--- test/echidna/EchidnaAgent.sol
+++ test/echidna/EchidnaAgent.sol
@@ -78,4 +78,336 @@ contract EchidnaAgent is EchidnaSetup {
             assert(false);
         }
     }
+
+    /////////////
+
+    function test_agent_borrow_fuzz(uint256 borrowAmount) public {
+        borrowAmount = bound(borrowAmount, 1, MAX_FIL);
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, borrowAmount + WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: borrowAmount}(INVESTOR);
+
+        uint256 liquidationValue = borrowAmount * 2;
+        hevm.deal(address(agent), liquidationValue);
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+        uint256 poolBorrowableAssetsBefore = pool.totalBorrowableAssets();
+
+        hevm.prank(AGENT_OWNER);
+        agent.borrow(poolID, sc);
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+        uint256 poolBorrowableAssetsAfter = pool.totalBorrowableAssets();
+
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore + borrowAmount);
+        assert(poolBorrowableAssetsAfter == poolBorrowableAssetsBefore - borrowAmount);
+    }
+
+    // function test_agent_borrow_balance(uint256 borrowAmount) public {
+    //     borrowAmount = bound(borrowAmount, WAD, MAX_FIL);
+
+    //     IAgent agent = _configureAgent(AGENT_OWNER);
+    //     uint256 agentID = agent.id();
+
+    //     hevm.deal(INVESTOR, borrowAmount + WAD);
+    //     hevm.prank(INVESTOR);
+    //     pool.deposit{value: borrowAmount}(INVESTOR);
+
+    //     uint256 liquidationValue = borrowAmount * 2;
+    //     hevm.deal(address(agent), liquidationValue);
+    //     SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+    //     uint256 agentBalanceBefore = address(agent).balance;
+    //     uint256 poolBalanceBefore = address(pool).balance;
+
+    //     hevm.prank(AGENT_OWNER);
+    //     agent.borrow(poolID, sc);
+
+    //     uint256 agentBalanceAfter = address(agent).balance;
+    //     uint256 poolBalanceAfter = address(pool).balance;
+
+    //     assert(agentBalanceAfter == agentBalanceBefore + borrowAmount);
+    //     assert(poolBalanceAfter == poolBalanceBefore - borrowAmount);
+    // }
+
+    function test_agent_borrow_min(uint256 borrowAmount) public {
+        borrowAmount = bound(borrowAmount, 1, WAD);
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, borrowAmount + WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: borrowAmount}(INVESTOR);
+
+        uint256 liquidationValue = borrowAmount * 2;
+        hevm.deal(address(agent), liquidationValue);
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+
+        hevm.prank(AGENT_OWNER);
+        agent.borrow(poolID, sc);
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore + borrowAmount);
+    }
+
+    function test_agent_borrow_large(uint256 borrowAmount) public {
+        borrowAmount = bound(borrowAmount, MAX_FIL / 2, MAX_FIL);
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, borrowAmount + WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: borrowAmount}(INVESTOR);
+
+        uint256 liquidationValue = borrowAmount * 2;
+        hevm.deal(address(agent), liquidationValue);
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+
+        hevm.prank(AGENT_OWNER);
+        agent.borrow(poolID, sc);
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore + borrowAmount);
+    }
+
+    function test_agent_borrow_small(uint256 borrowAmount) public {
+        borrowAmount = bound(borrowAmount, 1, WAD / 2);
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, borrowAmount + WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: borrowAmount}(INVESTOR);
+
+        uint256 liquidationValue = borrowAmount * 2;
+        hevm.deal(address(agent), liquidationValue);
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+
+        hevm.prank(AGENT_OWNER);
+        agent.borrow(poolID, sc);
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore + borrowAmount);
+    }
+
+    // function test_agent_borrow_insufficient_liquidity(uint256 borrowAmount) public {
+    //     borrowAmount = bound(borrowAmount, MAX_FIL + 1, MAX_FIL * 2);
+    //     Debugger.log("Borrow Amount", borrowAmount);
+
+    //     IAgent agent = _configureAgent(AGENT_OWNER);
+    //     uint256 agentID = agent.id();
+    //     Debugger.log("Agent ID", agentID);
+
+    //     hevm.deal(INVESTOR, MAX_FIL);
+    //     hevm.prank(INVESTOR);
+    //     pool.deposit{value: MAX_FIL}(INVESTOR);
+    //     Debugger.log("Investor Balance After Deposit", INVESTOR.balance);
+
+    //     uint256 liquidationValue = borrowAmount * 2;
+    //     hevm.deal(address(agent), liquidationValue);
+    //     Debugger.log("Agent Balance After Liquidation Deal", address(agent).balance);
+
+    //     SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+    //     Debugger.log("SignedCredential vc.subject", sc.vc.subject);
+    //     Debugger.log("SignedCredential vc.value", sc.vc.value);
+    //     Debugger.log("SignedCredential vc.target", sc.vc.target);
+    //     Debugger.log("SignedCredential vc.issuer", uint256(uint160(sc.vc.issuer)));
+    //     Debugger.log("SignedCredential v", sc.v);
+    //     Debugger.log("SignedCredential r", uint256(sc.r));
+    //     Debugger.log("SignedCredential s", uint256(sc.s));
+
+    //     uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+    //     Debugger.log("Agent Liquid Assets Before", agentLiquidAssetsBefore);
+
+    //     bool revertExpected = false;
+
+    //     hevm.prank(AGENT_OWNER);
+    //     try agent.borrow(poolID, sc) {
+    //         revertExpected = true;
+    //     } catch {
+    //         revertExpected = false;
+    //     }
+
+    //     Debugger.log("Revert Expected", revertExpected);
+
+    //     uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+    //     Debugger.log("Agent Liquid Assets After", agentLiquidAssetsAfter);
+
+    //     assert(!revertExpected);
+    //     assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore);
+    // }
+
+    function test_agent_borrow_invalid_params(uint256 borrowAmount) public {
+        borrowAmount = bound(borrowAmount, 0, WAD - 1);
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: WAD}(INVESTOR);
+
+        uint256 liquidationValue = borrowAmount * 2;
+        hevm.deal(address(agent), liquidationValue);
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+
+        hevm.prank(AGENT_OWNER);
+        try agent.borrow(poolID, sc) {
+            assert(false);
+        } catch {}
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore);
+    }
+
+    function test_agent_borrow_not_owner(uint256 borrowAmount) public {
+        borrowAmount = bound(borrowAmount, WAD, MAX_FIL);
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, borrowAmount + WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: borrowAmount}(INVESTOR);
+
+        uint256 liquidationValue = borrowAmount * 2;
+        hevm.deal(address(agent), liquidationValue);
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+
+        try agent.borrow(poolID, sc) {
+            assert(false);
+        } catch {}
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore);
+    }
+
+    function test_agent_borrow_expired_credential() public {
+        uint256 borrowAmount = WAD;
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, borrowAmount + WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: borrowAmount}(INVESTOR);
+
+        uint256 liquidationValue = borrowAmount * 2;
+        hevm.deal(address(agent), liquidationValue);
+
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+        sc.vc.epochValidUntil = block.number - 1;
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+
+        bool revertExpected = false;
+
+        hevm.prank(AGENT_OWNER);
+        try agent.borrow(poolID, sc) {
+            revertExpected = false;
+        } catch {
+            revertExpected = true;
+        }
+
+        Debugger.log("Revert Expected", revertExpected);
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+        Debugger.log("Agent Liquid Assets After", agentLiquidAssetsAfter);
+
+        assert(revertExpected);
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore);
+    }
+
+    function test_agent_borrow_invalid_signature() public {
+        uint256 borrowAmount = WAD;
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, borrowAmount + WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: borrowAmount}(INVESTOR);
+
+        uint256 liquidationValue = borrowAmount * 2;
+        hevm.deal(address(agent), liquidationValue);
+
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+        sc.s = bytes32(0);
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+
+        bool revertExpected = false;
+
+        hevm.prank(AGENT_OWNER);
+        try agent.borrow(poolID, sc) {
+            revertExpected = false;
+        } catch {
+            revertExpected = true;
+        }
+
+        Debugger.log("Revert Expected", revertExpected);
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+        Debugger.log("Agent Liquid Assets After", agentLiquidAssetsAfter);
+
+        assert(revertExpected);
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore);
+    }
+
+    function test_agent_borrow_update_epochs_paid(uint256 borrowAmount, uint256 existingPrincipal) public {
+        borrowAmount = bound(borrowAmount, WAD, MAX_FIL);
+        existingPrincipal = bound(existingPrincipal, WAD, MAX_FIL);
+
+        IAgent agent = _configureAgent(AGENT_OWNER);
+        uint256 agentID = agent.id();
+
+        hevm.deal(INVESTOR, borrowAmount + existingPrincipal + WAD);
+        hevm.prank(INVESTOR);
+        pool.deposit{value: borrowAmount + existingPrincipal}(INVESTOR);
+
+        uint256 liquidationValue = (borrowAmount + existingPrincipal) * 2;
+        hevm.deal(address(agent), liquidationValue);
+
+        SignedCredential memory scInitial = _issueBorrowCred(agentID, existingPrincipal, liquidationValue);
+        hevm.prank(AGENT_OWNER);
+        agent.borrow(poolID, scInitial);
+
+        SignedCredential memory sc = _issueBorrowCred(agentID, borrowAmount, liquidationValue);
+
+        uint256 agentLiquidAssetsBefore = agent.liquidAssets();
+        uint256 totalBorrowedBefore = pool.totalBorrowed();
+
+        hevm.prank(AGENT_OWNER);
+        agent.borrow(poolID, sc);
+
+        uint256 agentLiquidAssetsAfter = agent.liquidAssets();
+        uint256 totalBorrowedAfter = pool.totalBorrowed();
+
+        assert(agentLiquidAssetsAfter == agentLiquidAssetsBefore + borrowAmount);
+        assert(totalBorrowedAfter == totalBorrowedBefore + borrowAmount);
+    }
 }
